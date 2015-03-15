a06d124cde2fa7d043a7897a16bfc6c28bfd8600
Brandon Rossi <brandonrossisjsu@gmail.com>
2015-03-14 18:28:18 -0700

    Able to put customer, and get customers

--- src/brandon/cs157/restful/orderApplication/Restful.java
+++ src/brandon/cs157/restful/orderApplication/Restful.java
@@ -1,32 +1,61 @@
 package brandon.cs157.restful.orderApplication;
 
+import java.util.List;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/store")
 public class Restful 
 {
-	OrderEntryService orderEntryService = new OrderEntryService();
+	
+	private static final Hw2DAO hw2dao = new Hw2DAO();
+	
+	
 	
 	@Path("/customer/{customerName}")
 	@POST
 	@Produces(MediaType.TEXT_PLAIN)
-	public String createNewCustomer(@PathParam("customerName") String customerName)
+	public void addCustomer(@PathParam("customerName") String customerName)
+	{
+		Customer customer = new Customer();
+		customer.setName(customerName);
+		hw2dao.persistObject(customer);
+	}
+	
+	@Path("/customer")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCustomers()
 	{
 		
-		orderEntryService.addCustomer(customerName);
-		return "You Have Been Added TO The Store Database";
-		 
+		StringBuilder stringBuilder =  new StringBuilder();
+		List<Customer> customers = hw2dao.getCustomers();
+		if(customers.isEmpty())
+		{
+			return "There Are Currently No Customers In The Database"; 
+		}
+		for (Customer customer : customers) {
+			stringBuilder.append(customer.toString());
+		}
+		return stringBuilder.toString();
 	}
 	
 	@Path("/orders")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getCustomerOrders()
+	public String getAllOrders()
 	{
-		return orderEntryService.getAllOrders();
+		List<CustomerOrder> customerOrders = hw2dao.getCustomerOrders();
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for (CustomerOrder customerOrder : customerOrders) 
+		{
+			stringBuilder.append(customerOrder.toString());
+			stringBuilder.append("\n");
+		}
+		
+		return stringBuilder.toString();
 	}
-	
-	
-
 }
